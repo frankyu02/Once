ae61ded7f50de9c03c8a37faf521c4940c81c899
frankyu02 <frankyu02@yahoo.ca>
2022-10-19 13:45:14 -0400

    SEO created

---
+++ Components/SEO.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+export default function SEO() {
+  return (
+    <Head>
+      <title>Once - Frank Yu</title>
+      <meta
+        name="description"
+        content="If you can only post one thing a day, wouldn't it be the most meaningful thing you'll say all day?"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
