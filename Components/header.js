f8afedddfa09c9bb6d0dce46fb921ba4f536ada4
frankyu02 <frankyu02@yahoo.ca>
2022-10-19 13:44:41 -0400

    basic header

--- Components/header.js
+++ Components/header.js
@@ -1,15 +1,33 @@
 import react from "react";
 import styled from "styled-components";
+import { AiFillGithub } from "react-icons/ai";
 
 const Wrapper = styled.div`
   border: 1px solid black;
   width: 100%;
   background: var(--Background);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    color: var(--Headline);
+  }
+  a {
+    font-size: 1.5rem;
+  }
 `;
 export default function Header() {
   return (
     <Wrapper>
       <h1>Once</h1>
+      <a
+        href="https://github.com/frankyu02/Once"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiFillGithub />
+      </a>
     </Wrapper>
   );
 }
