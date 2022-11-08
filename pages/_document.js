65a67d8b2667257458aa7566f5015ab11293b06f
frankyu02 <frankyu02@yahoo.ca>
2022-11-08 00:02:48 -0500

    added link for fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,59 @@
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet, createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+ html,
+:root{
+  --Background: #f2f7f5;
+  --Headline: #00473e;
+  --Paragraph: #475d5b;
+  --Button: #faae2b;
+  --ButtonText: #00473e;
+}
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
 
-export default class MyDocument extends Document {
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+@media (prefers-color-scheme: dark) {
+  html {
+    color-scheme: dark;
+  }
+  body {
+    color: white;
+    background: black;
+  }
+}
+`;
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
+          // useful for wrapping the whole react tree
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+            sheet.collectStyles(
+              <>
+                <GlobalStyle />
+                <App {...props} />
+              </>
+            ),
+          // useful for wrapping in a per-page basis
+          enhanceComponent: (Component) => Component,
         });
-
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
@@ -27,4 +68,23 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Xanh+Mono:ital@1&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+export default MyDocument;
