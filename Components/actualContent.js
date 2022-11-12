8fb380d50ac5999bb2f017458e95da9d451a484e
frankyu02 <frankyu02@yahoo.ca>
2022-11-11 21:40:21 -0500

    creating area to do actual content

---
+++ Components/actualContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+    width: 100%;
+    outline: 5px solid green;
+    height: calc(100vh - 150px);
+`
+export default function ActualContent() {
+    return (
+        <Wrapper>
+        </Wrapper>
+    );
+}
