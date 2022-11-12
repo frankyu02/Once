477c56ae37e0a98c000797c47fab56686d008d2d
frankyu02 <frankyu02@yahoo.ca>
2022-11-11 21:41:52 -0500

    user image

--- Components/headerSideBar.js
+++ Components/headerSideBar.js
@@ -1,13 +1,23 @@
+import Image from "next/image";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import firebase from "../Firebase/clientApp";
-
 export default function HeaderSideBar() {
     const handleSignOut = () => {
         firebase.auth().signOut();
     }
+    const [user] = useAuthState(firebase.auth());
+    console.log(user);
     return (
         <div className="logOut">
-            <button onClick={handleSignOut}>Log Out</button>
+            {user ?
+                <div className="userSummary">
+                    <Image src={user.photoURL} alt="user Image" width="50%" height="100%" />
+                </div>
+                : ''}
+            <div className="buttonDiv">
+                <button onClick={handleSignOut}>Log Out</button>
+            </div>
         </div>
     );
 }
