87b76650d99cd6ed71baf8dcea699dc41daea274
frankyu02 <frankyu02@yahoo.ca>
2022-12-03 13:36:19 -0500

    added emulator support

--- Firebase/clientApp.js
+++ Firebase/clientApp.js
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
-const firebaseConfig = {
+let firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECTID,
@@ -17,5 +17,5 @@ const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-
+connectFirestoreEmulator(db, "http://127.0.0.1/", 8080);
 export { db, auth, firebase };
