e074df882065e21082cbfb6d5c59533b7308cbd4
frankyu02 <frankyu02@yahoo.ca>
2022-12-04 16:09:38 -0500

    for inputBox and posts

--- Components/actualContent.js
+++ Components/actualContent.js
@@ -1,14 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import InputBox from "./Actual Content Coponents/inputBox";
+import PostsBox from "./Actual Content Coponents/posts";
 
 const Wrapper = styled.section`
-    width: 100%;
-    outline: 5px solid green;
-    height: calc(100vh - 150px);
-`
-export default function ActualContent() {
-    return (
-        <Wrapper>
-        </Wrapper>
-    );
+  width: 100%;
+  outline: 5px solid green;
+  height: calc(100vh - 150px);
+  background: var(--Background);
+`;
+export default function ActualContent({ user }) {
+  return (
+    <Wrapper>
+      <PostsBox />
+      <InputBox user={user} />
+    </Wrapper>
+  );
 }
