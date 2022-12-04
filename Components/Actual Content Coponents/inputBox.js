a3408fb688326a4d6729844c6490c5095a1cc9a9
frankyu02 <frankyu02@yahoo.ca>
2022-12-04 16:09:19 -0500

    box for inputting post data

---
+++ Components/Actual Content Coponents/inputBox.js
@@ -0,0 +1,114 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import react, { useState } from "react";
+import styled from "styled-components";
+import { db } from "../../Firebase/clientApp";
+import { useDocument } from "react-firebase-hooks/firestore";
+
+const Wrapper = styled.div`
+  height: 10%;
+  width: 100%;
+  outline: 5px solid purple;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  input {
+    background: white;
+    width: 80%;
+    height: 80%;
+  }
+  input,
+  select,
+  textarea {
+    color: var(--Paragraph);
+  }
+  .no-post-textbox {
+    opacity: 50%;
+    pointer-events: none;
+    border: none;
+  }
+  button {
+    width: 10%;
+    height: 80%;
+    background-color: var(--Button);
+    color: var(--ButtonText);
+    &:hover {
+      cursor: pointer;
+    }
+    &:disabled {
+      cursor: default;
+      opacity: 50%;
+    }
+  }
+`;
+const canUserPost = (user) => {
+  if (!user?.exists()) {
+    return true;
+  } else {
+    const data = user?.data();
+    const date = data?.LastPosted?.toDate();
+    const now = new Date();
+    const msBetweenDates = Math.abs(date?.getTime() - now.getTime());
+    const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
+    return hoursBetweenDates >= 24;
+  }
+};
+export default function InputBox({ user }) {
+  const [sending, setSending] = useState(false);
+  const [text, setText] = useState("");
+  const [dbUser, loadingUser, errorUser] = useDocument(
+    doc(db, "users", user.uid)
+  );
+  let canPost;
+  if (!loadingUser) {
+    canPost = canUserPost(dbUser);
+  }
+  const handleTextChange = (e) => {
+    e.preventDefault();
+    setText(e.target.value);
+  };
+  const sendData = async () => {
+    const sendingData = {
+      createdAt: serverTimestamp(),
+      createdBy: user.displayName,
+      createdProfile: user.photoURL,
+      text: text,
+    };
+    setSending(true);
+    await addDoc(collection(db, "posts"), sendingData)
+      .then(async () => {
+        await setDoc(doc(db, "users", user.uid), {
+          LastPosted: serverTimestamp(),
+        });
+      })
+      .finally(() => {
+        setSending(false);
+        setText("");
+      });
+  };
+  const disabled = sending || text.length <= 0 || loadingUser || !canPost;
+  return (
+    <Wrapper>
+      <input
+        type="text"
+        value={text}
+        onChange={handleTextChange}
+        tabIndex="-1"
+        placeholder={
+          canPost
+            ? "Hey Lets make a post!"
+            : "Sorry, only one post is allowd per day!"
+        }
+        className={canPost ? "" : "no-post-textbox"}
+      />
+      <button disabled={disabled} onClick={sendData}>
+        post
+      </button>
+    </Wrapper>
+  );
+}
