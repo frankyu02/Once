d3ddea8910a50d3ea555a94c1dbfd7eb3a6f349a
frankyu02 <frankyu02@yahoo.ca>
2023-01-16 16:40:42 -0500

    show today's date only

--- Components/Actual Content Coponents/posts.js
+++ Components/Actual Content Coponents/posts.js
@@ -2,7 +2,13 @@ import react from "react";
 import styled from "styled-components";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../../Firebase/clientApp";
-import { collection, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  orderBy,
+  query,
+  Timestamp,
+  where,
+} from "firebase/firestore";
 import PostBox from "./postBox";
 const Wrapper = styled.div`
   width: 100%;
@@ -11,14 +17,29 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  h1 {
+    color: var(--Headline);
+  }
 `;
+const getToday = () => {
+  const now = new Date();
+  now.setHours(5, 0, 0, 0);
+  const timestamp = Timestamp.fromDate(now);
+  return timestamp;
+};
 export default function PostsBox() {
   const postsRef = collection(db, "posts");
-  const q = query(postsRef, orderBy("createdAt", "desc"));
+  const q = query(
+    postsRef,
+    where("createdAt", ">", getToday()),
+    orderBy("createdAt", "desc")
+  );
   const [posts, postsLoading, postsError] = useCollection(q);
   const postsData = posts?.docs;
+  const today = new Date();
   return (
     <Wrapper>
+      <h1>Posts from {today.toDateString()}</h1>
       {postsError && <div>Something went Wrong with loading posts</div>}
       {postsLoading && <div>loading posts...</div>}
       {posts &&
